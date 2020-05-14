94603d1357c110d78a3c2480166e323ed0749b8e
Dreamwalker <aristojeff@gmail.com>
2020-05-14 21:28:46 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutternotebook6thstory/ep451_email_app/email_list_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(EmailApp());
 }

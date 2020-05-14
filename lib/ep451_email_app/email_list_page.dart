29d237959fb2502150111921063b823e41deb7d9
Dreamwalker <aristojeff@gmail.com>
2020-05-14 21:39:05 +0900

    Update email_list_page.dart

--- lib/ep451_email_app/email_list_page.dart
+++ lib/ep451_email_app/email_list_page.dart
@@ -68,6 +68,11 @@ class _EmailListPageState extends State<EmailListPage> {
                         )
                       ]
                   ),
+                  child: Center(
+                    child: AnimatedIcon(progress: null,
+                      
+                    ),
+                  ),
                 )
               ],
             ),

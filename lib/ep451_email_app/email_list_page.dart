e1e10fbd3767f515f5452ad0db408dfc9e3e70a2
Dreamwalker <aristojeff@gmail.com>
2020-05-14 21:28:24 +0900

    Update email_list_page.dart

---
+++ lib/ep451_email_app/email_list_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class EmailApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/" : (context) =>  
+      },
+    );
+  }
+}
+
+class EmailListPage extends StatefulWidget {
+  @override
+  _EmailListPageState createState() => _EmailListPageState();
+}
+
+class _EmailListPageState extends State<EmailListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+

7b007fc4037c682b1aa52e77353d91944159af6d
Dreamwalker <aristojeff@gmail.com>
2020-05-15 19:39:47 +0900

    Update email_list_page.dart

--- lib/ep451_email_app/email_list_page.dart
+++ lib/ep451_email_app/email_list_page.dart
@@ -35,91 +35,95 @@ class _EmailListPageState extends State<EmailListPage>with SingleTickerProviderS
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-              left: 0,
-              right: 0,
-              top: 100,
-              bottom: 0,
-              child: ListView.builder(itemBuilder: (context, index){
-
-                return Slidable(actionPane: SlidableBehindActionPane(), child: Container(height: 94,
-                  color: Colors.red, margin: EdgeInsets.only(bottom: 8),),);
-              })
-          ),
-          Positioned(
-            right: 24,
-            bottom: 24,
-            child: Container(
-              height: 84,
-              width: 84,
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    spreadRadius: 3,
-                    offset: Offset(0,2)
-                  )
-                ]
-              ),
-              child: Center(
-                child: Text('CHAT', style: GoogleFonts.anton(
-                  letterSpacing: 2,
-                  fontSize: 20
-                ),),
+    return Theme(
+      data: ThemeData(
+        accentColor: Colors.grey[100]
+      ),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Positioned(
+                left: 0,
+                right: 0,
+                top: 100,
+                bottom: 0,
+                child: ListView.builder(itemBuilder: (context, index){
+                  return Slidable(actionPane: SlidableBehindActionPane(), child: Container(height: 94,
+                    color: Colors.red, margin: EdgeInsets.only(bottom: 8),),);
+                })
+            ),
+            Positioned(
+              right: 24,
+              bottom: 24,
+              child: Container(
+                height: 84,
+                width: 84,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      spreadRadius: 3,
+                      offset: Offset(0,2)
+                    )
+                  ]
+                ),
+                child: Center(
+                  child: Text('CHAT', style: GoogleFonts.anton(
+                    letterSpacing: 2,
+                    fontSize: 20
+                  ),),
+                ),
               ),
             ),
-          ),
-          Positioned(
-            left: 16,
-            right: 16,
-            top: 32,
-            child: Row(
-              children: [
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black,
-                            spreadRadius: 2,
-                            offset: Offset(0,2)
-                        )
-                      ]
-                  ),
-                  child: Center(
-                    child:
-                    IconButton(
-                      onPressed: (){
-                        isClicked = !isClicked;
-                        isClicked ? controller.forward() : controller.reverse();
-                      },
-                      icon: AnimatedIcon(progress:controller,
-                        icon: AnimatedIcons.menu_close,
+            Positioned(
+              left: 16,
+              right: 16,
+              top: 32,
+              child: Row(
+                children: [
+                  Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black,
+                              spreadRadius: 2,
+                              offset: Offset(0,2)
+                          )
+                        ]
+                    ),
+                    child: Center(
+                      child:
+                      IconButton(
+                        onPressed: (){
+                          isClicked = !isClicked;
+                          isClicked ? controller.forward() : controller.reverse();
+                        },
+                        icon: AnimatedIcon(progress:controller,
+                          icon: AnimatedIcons.menu_close,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text("Inbox",style: GoogleFonts.anton(
-                    fontSize: 24,
-                    letterSpacing: 1
-                  ),),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text("Inbox",style: GoogleFonts.anton(
+                      fontSize: 24,
+                      letterSpacing: 1
+                    ),),
+                  )
+                ],
+              ),
             ),
-          ),
 
-          
-        ],
+
+          ],
+        ),
       ),
     );
   }

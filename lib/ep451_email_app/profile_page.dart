7754995288138745bfe094aff77663a9bbaec0db
Dreamwalker <aristojeff@gmail.com>
2020-05-16 19:11:02 +0900

    Update profile_page.dart

--- lib/ep451_email_app/profile_page.dart
+++ lib/ep451_email_app/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -11,7 +12,37 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       body: Column(
         children: [
-          
+          Row(
+            children: [
+              Container(
+                height: 42,
+                width: 42,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    boxShadow: [BoxShadow(color: Colors.black, spreadRadius: 2, offset: Offset(0, 2))]),
+                child: Center(
+                  child: IconButton(
+                    onPressed: () {
+                      isClicked = !isClicked;
+                      isClicked ? controller.forward() : controller.reverse();
+                    },
+                    icon: AnimatedIcon(
+                      progress: controller,
+                      icon: AnimatedIcons.menu_close,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Inbox",
+                  style: GoogleFonts.anton(fontSize: 24, letterSpacing: 1),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );

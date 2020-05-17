3581a3ce514da83e32a21e15ca162be7fee3dde0
Dreamwalker <aristojeff@gmail.com>
2020-05-17 20:47:12 +0900

    Update online_home_page.dart

--- lib/ep454_online_course_app/online_home_page.dart
+++ lib/ep454_online_course_app/online_home_page.dart
@@ -13,27 +13,22 @@ class _OnlineHomePageState extends State<OnlineHomePage> {
         child: Column(
           children: [
             Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(Icons.menu),
-                    Icon(Icons.search)
-                  ],
-                ),
-              )
-            ),
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Icon(Icons.menu), Icon(Icons.search)],
+                  ),
+                )),
             Expanded(
               flex: 6,
-              child: Placeholder(),
+              child: Column(),
             ),
             Expanded(
               flex: 10,
               child: Placeholder(),
             )
-
           ],
         ),
       ),

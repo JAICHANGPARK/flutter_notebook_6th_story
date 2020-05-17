78f9bfe3f9a03b0fdf964d902114c00efad403a0
Dreamwalker <aristojeff@gmail.com>
2020-05-17 20:44:04 +0900

    Update online_app_main.dart

--- lib/ep454_online_course_app/online_app_main.dart
+++ lib/ep454_online_course_app/online_app_main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class OnlineCourseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: OnlineHomePage(),
+    );
   }
 }

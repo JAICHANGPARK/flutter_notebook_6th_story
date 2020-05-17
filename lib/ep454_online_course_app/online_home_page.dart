ac2bcf68c7ed71c608bc984635197022383b77b7
Dreamwalker <aristojeff@gmail.com>
2020-05-17 20:44:40 +0900

    Update online_home_page.dart

--- lib/ep454_online_course_app/online_home_page.dart
+++ lib/ep454_online_course_app/online_home_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class OnlineHomePage extends StatefulWidget {
+  @override
+  _OnlineHomePageState createState() => _OnlineHomePageState();
+}
+
+class _OnlineHomePageState extends State<OnlineHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

9422ea7b3f7ca0c9ffadc56c465da78f140b3db7
Dreamwalker <aristojeff@gmail.com>
2020-05-22 20:03:47 +0900

    Create time_setting_page.dart

---
+++ lib/ep457_apple_breathe_app/time_setting_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TimeSettingPage extends StatefulWidget {
+  @override
+  _TimeSettingPageState createState() => _TimeSettingPageState();
+}
+
+class _TimeSettingPageState extends State<TimeSettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+

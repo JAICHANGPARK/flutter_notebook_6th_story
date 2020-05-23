a27cb515559dd3ced406b0442e4611734a36ab7c
Dreamwalker <aristojeff@gmail.com>
2020-05-23 18:23:03 +0900

    Update market_main_pae.dart

--- lib/ep460_marketplace_app/market_main_pae.dart
+++ lib/ep460_marketplace_app/market_main_pae.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class MarketPlaceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: MarketMainPage(),
+    );
   }
 }

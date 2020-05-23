15be418d6f868faeaf36bd16f59838259806eede
Dreamwalker <aristojeff@gmail.com>
2020-05-23 18:29:04 +0900

    Update market_main_pae.dart

--- lib/ep460_marketplace_app/market_main_pae.dart
+++ lib/ep460_marketplace_app/market_main_pae.dart
@@ -24,10 +24,14 @@ class _MarketMainpageState extends State<MarketMainPage> {
           Expanded(
             flex: 1,
             child: Container(
-              color: Colors.yellow,
               child: Column(
                 children: [
-
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Expanded(
+                    child: Placeholder(),
+                  )
                 ],
               ),
             ),

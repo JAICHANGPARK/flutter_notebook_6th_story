3d02d1ca67c911dc63ba27a6784cbae0a69946b9
Dreamwalker <aristojeff@gmail.com>
2020-05-25 19:54:04 +0900

    Update market_main_pae.dart

--- lib/ep460_marketplace_app/market_main_pae.dart
+++ lib/ep460_marketplace_app/market_main_pae.dart
@@ -226,12 +226,9 @@ class _MarketMainpageState extends State<MarketMainPage> {
                                       ),
                                       Expanded(
                                         child: Container(
-                                          margin: EdgeInsets.symmetric(horizontal: 8),
-                                          decoration: BoxDecoration(
-                                          border: Border.all(),borderRadius: BorderRadius.circular(36)
-
-                                          )
-                                        ),
+                                            margin: EdgeInsets.symmetric(horizontal: 8),
+                                            decoration: BoxDecoration(
+                                                border: Border.all(), borderRadius: BorderRadius.circular(36))),
                                       ),
                                     ],
                                   ),

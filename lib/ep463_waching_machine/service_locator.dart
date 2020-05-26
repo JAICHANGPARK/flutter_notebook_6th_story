3eb6dfeb242fc91530b9e7e43962edc48c322757
Dreamwalker <aristojeff@gmail.com>
2020-05-26 20:57:21 +0900

    Update service_locator.dart

--- lib/ep463_waching_machine/service_locator.dart
+++ lib/ep463_waching_machine/service_locator.dart
@@ -1,17 +1,14 @@
-
-
 import 'package:flutternotebook6thstory/ep463_waching_machine/providers/theme_provider.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt getIt = GetIt.instance;
 
-
-class ServiceLocator{
-  static init(){
+class ServiceLocator {
+  static init() {
     getIt.registerSingleton<ThemeProvider>(ThemeProvider());
   }
 
-  static T get<T>(){
+  static T get<T>() {
     return getIt.get<T>();
   }
 }

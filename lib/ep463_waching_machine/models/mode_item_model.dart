948944470391891a6f33171d0bb5185796e28014
Dreamwalker <aristojeff@gmail.com>
2020-05-26 20:46:30 +0900

    Create mode_item_model.dart

---
+++ lib/ep463_waching_machine/models/mode_item_model.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:flutter/material.dart';
+
+class ModeItemModel{
+  final String name;
+  final int minutes;
+  final Color color;
+
+  ModeItemModel({this.name, this.minutes, this.color});
+}

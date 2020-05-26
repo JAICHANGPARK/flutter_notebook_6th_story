b955fa26d8f13ebe65c9e5c58bafc0cabf24fbc7
Dreamwalker <aristojeff@gmail.com>
2020-05-26 20:54:19 +0900

    Update theme_provider.dart

--- lib/ep463_waching_machine/providers/theme_provider.dart
+++ lib/ep463_waching_machine/providers/theme_provider.dart
@@ -6,4 +6,13 @@ import 'package:flutter/foundation.dart';
 
 class ThemeProvider with ChangeNotifier{
 
+  bool _darkMode = false;
+  bool get darkMode => _darkMode;
+  set darkMode(bool value){
+    if(_darkMode == value){
+      return;
+    }
+    _darkMode = value;
+    notifyListeners();
+  }
 }

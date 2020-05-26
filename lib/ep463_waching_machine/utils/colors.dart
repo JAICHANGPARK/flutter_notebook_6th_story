0b9dee01503fd9bc11037907af696131294d4a2a
Dreamwalker <aristojeff@gmail.com>
2020-05-26 21:10:39 +0900

    Update colors.dart

--- lib/ep463_waching_machine/utils/colors.dart
+++ lib/ep463_waching_machine/utils/colors.dart
@@ -4,51 +4,49 @@ import 'package:flutternotebook6thstory/ep463_waching_machine/providers/theme_pr
 
 import '../service_locator.dart';
 
-
 class CustomColors {
-  static bool get isDarkMode =>
-      ServiceLocator.get<ThemeProvider>()?.darkMode ?? false;
+  static bool get isDarkMode => ServiceLocator.get<ThemeProvider>()?.darkMode ?? false;
 
-  static Color get primaryColor =>
-      isDarkMode ? _primaryColorDark : _primaryColorLight;
+  static Color get primaryColor => isDarkMode ? _primaryColorDark : _primaryColorLight;
+
   static Color get container => isDarkMode ? _containerDark : _containerLight;
-  static Color get containerPressed =>
-      isDarkMode ? _containerPressedDark : _containerPressedLight;
-  static Color get containerShadowTop =>
-      isDarkMode ? _containerShadowTopDark : _containerShadowTopLight;
-  static Color get containerShadowBottom =>
-      isDarkMode ? _containerShadowBottomDark : _containerShadowBottomLight;
-  static Color get containerInnerShadowTop =>
-      isDarkMode ? _containerInnerShadowTopDark : _containerInnerShadowTopLight;
-  static Color get containerInnerShadowBottom => isDarkMode
-      ? _containerInnerShadowBottomDark
-      : _containerInnerShadowBottomLight;
-  static Color get primaryTextColor =>
-      isDarkMode ? _primaryTextColorDark : _primaryTextColorLight;
-  static Color get secondaryTextColor =>
-      isDarkMode ? _secondaryTextColorDark : _secondaryTextColorLight;
-  static Color get icon => isDarkMode ? _iconDark : _iconLight;
-  static Color get indicatorBackground =>
-      isDarkMode ? _indicatorBackgroundDark : _indicatorBackgroundLight;
-  static Color get timerPanelBorder =>
-      isDarkMode ? _timerPanelBorderDark : _timerPanelBorderLight;
 
-  static Color get drumBorder =>
-      isDarkMode ? _drumBorderDark : _drumBorderLight;
-  static Color get drumBackground =>
-      isDarkMode ? _drumBackgroundDark : _drumBackgroundLight;
-  static Color get drumRibBackground =>
-      isDarkMode ? _drumRibBackgroundDark : _drumRibBackgroundLight;
-  static Color get drumRibForeground =>
-      isDarkMode ? _drumRibForegroundDark : _drumRibForegroundLight;
-  static List<Color> get drumInnerShadowColors =>
-      isDarkMode ? _drumInnerShadowColorsDark : _drumInnerShadowColorsLight;
-  static List<Color> get drumRing1Colors =>
-      isDarkMode ? _drumRing1ColorsDark : _drumRing1ColorsLight;
-  static List<Color> get drumRing2Colors =>
-      isDarkMode ? _drumRing2ColorsDark : _drumRing2ColorsLight;
-  static List<Color> get drumRing3Colors =>
-      isDarkMode ? _drumRing3ColorsDark : _drumRing3ColorsLight;
+  static Color get containerPressed => isDarkMode ? _containerPressedDark : _containerPressedLight;
+
+  static Color get containerShadowTop => isDarkMode ? _containerShadowTopDark : _containerShadowTopLight;
+
+  static Color get containerShadowBottom => isDarkMode ? _containerShadowBottomDark : _containerShadowBottomLight;
+
+  static Color get containerInnerShadowTop => isDarkMode ? _containerInnerShadowTopDark : _containerInnerShadowTopLight;
+
+  static Color get containerInnerShadowBottom =>
+      isDarkMode ? _containerInnerShadowBottomDark : _containerInnerShadowBottomLight;
+
+  static Color get primaryTextColor => isDarkMode ? _primaryTextColorDark : _primaryTextColorLight;
+
+  static Color get secondaryTextColor => isDarkMode ? _secondaryTextColorDark : _secondaryTextColorLight;
+
+  static Color get icon => isDarkMode ? _iconDark : _iconLight;
+
+  static Color get indicatorBackground => isDarkMode ? _indicatorBackgroundDark : _indicatorBackgroundLight;
+
+  static Color get timerPanelBorder => isDarkMode ? _timerPanelBorderDark : _timerPanelBorderLight;
+
+  static Color get drumBorder => isDarkMode ? _drumBorderDark : _drumBorderLight;
+
+  static Color get drumBackground => isDarkMode ? _drumBackgroundDark : _drumBackgroundLight;
+
+  static Color get drumRibBackground => isDarkMode ? _drumRibBackgroundDark : _drumRibBackgroundLight;
+
+  static Color get drumRibForeground => isDarkMode ? _drumRibForegroundDark : _drumRibForegroundLight;
+
+  static List<Color> get drumInnerShadowColors => isDarkMode ? _drumInnerShadowColorsDark : _drumInnerShadowColorsLight;
+
+  static List<Color> get drumRing1Colors => isDarkMode ? _drumRing1ColorsDark : _drumRing1ColorsLight;
+
+  static List<Color> get drumRing2Colors => isDarkMode ? _drumRing2ColorsDark : _drumRing2ColorsLight;
+
+  static List<Color> get drumRing3Colors => isDarkMode ? _drumRing3ColorsDark : _drumRing3ColorsLight;
 
   static Color _primaryColorLight = Color.fromRGBO(235, 240, 243, 1);
   static Color _primaryColorDark = Color.fromRGBO(32, 35, 41, 1);
@@ -68,8 +66,7 @@ class CustomColors {
   static Color _containerInnerShadowTopLight = Color.fromRGBO(204, 206, 212, 1);
   static Color _containerInnerShadowTopDark = Color.fromRGBO(0, 0, 0, .9);
 
-  static Color _containerInnerShadowBottomLight =
-  Color.fromRGBO(255, 255, 255, 1);
+  static Color _containerInnerShadowBottomLight = Color.fromRGBO(255, 255, 255, 1);
   static Color _containerInnerShadowBottomDark = Color.fromRGBO(49, 54, 60, 1);
 
   static Color _primaryTextColorLight = Color.fromRGBO(53, 54, 59, 1);

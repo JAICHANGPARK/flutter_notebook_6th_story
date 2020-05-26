6148ab3ed82c229e8c9f396421ffdc818016dcdc
Dreamwalker <aristojeff@gmail.com>
2020-05-26 21:05:11 +0900

    Update neumorphic_button.dart

--- lib/ep463_waching_machine/ui/neumorphic_button.dart
+++ lib/ep463_waching_machine/ui/neumorphic_button.dart
@@ -1,10 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 
 class NeumorphicButton extends StatefulWidget {
-
-
   final bool pressed;
   final double width;
   final double height;
@@ -14,16 +10,14 @@ class NeumorphicButton extends StatefulWidget {
   final Color color;
   final bool disabled;
 
-
   NeumorphicButton(
-      Key key,
-      this.pressed, this.width, this.height, this.child, this.margin, this.onTap, this.color, this.disabled);
+  { Key key, this.pressed, this.width, this.height, this.child, this.margin, this.onTap, this.color, this.disabled});
 
   @override
   _NeumorphicButtonState createState() => _NeumorphicButtonState();
 }
 
-class _NeumorphicButtonState extends State<NeumorphicButton > {
+class _NeumorphicButtonState extends State<NeumorphicButton> {
   @override
   Widget build(BuildContext context) {
     return Container();

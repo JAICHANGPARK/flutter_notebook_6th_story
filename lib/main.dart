d59712b9e9660a7b15d193bc8f38cc8883fa6b52
Dreamwalker <aristojeff@gmail.com>
2020-05-26 21:00:10 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutternotebook6thstory/ep463_waching_machine/service_locator.dart';
 import 'ep463_waching_machine/main_page.dart';
 
 void main() {
+  ServiceLocator.init();
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
   runApp(WashingMachineApp());
+
 }

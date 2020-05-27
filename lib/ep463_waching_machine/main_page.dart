21a03c06b570e43575bded1b5e6621013f2d0709
Dreamwalker <aristojeff@gmail.com>
2020-05-27 22:28:30 +0900

    Update main_page.dart

--- lib/ep463_waching_machine/main_page.dart
+++ lib/ep463_waching_machine/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/providers/theme_provider.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/service_locator.dart';
+import 'package:flutternotebook6thstory/ep463_waching_machine/ui/neumorphic_icon_button.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/utils/colors.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/utils/consts.dart';
 import 'package:provider/provider.dart';
@@ -35,7 +36,11 @@ class _WashingMachineMainPageState extends State<WashingMachineMainPage> {
       margin: EdgeInsets.fromLTRB(GLOBAL_EDGE_MARGIN_VALUE, DRAWER_BUTTON_MARGIN_TOP, 18, 10),
       child: Row(
         children: [
-
+            NeumorphicIconButton(
+              icon: Icon(Icons.settings,
+              color: CustomColors.icon,),
+            ),
+          
         ],
       ),
     );

e1ae679f1c4382bded2601ce06404dcd819637cb
Dreamwalker <aristojeff@gmail.com>
2020-05-27 22:31:37 +0900

    Update neumorphic_container.dart

--- lib/ep463_waching_machine/ui/neumorphic_container.dart
+++ lib/ep463_waching_machine/ui/neumorphic_container.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_whirlpool/shared/colors.dart';
+import 'package:flutter_whirlpool/shared/consts.dart';
+import 'package:flutter_whirlpool/shared/decorators.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/ui/inner_shadow_decorator.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/utils/colors.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/utils/consts.dart';
 
 class NeumorphicContainer extends StatelessWidget {
+  const NeumorphicContainer({
+    Key key,
+    this.child,
+    this.pressed,
+    this.width,
+    this.height,
+    this.margin,
+    this.padding,
+    this.color,
+    this.borderRadius,
+    this.border,
+    this.decoration,
+    this.foregroundDecoration,
+    this.disableForegroundDecoration,
+    this.disabled,
+  }) : super(key: key);
+
   final bool pressed;
   final double width;
   final double height;
@@ -14,37 +35,27 @@ class NeumorphicContainer extends StatelessWidget {
   final BoxBorder border;
   final Decoration decoration;
   final Decoration foregroundDecoration;
+  final EdgeInsetsGeometry padding;
   final bool disableForegroundDecoration;
   final bool disabled;
 
-  NeumorphicContainer(
-      {this.pressed,
-      this.width,
-      this.height,
-      this.child,
-      this.margin,
-      this.color,
-      this.borderRadius,
-      this.border,
-      this.decoration,
-      this.foregroundDecoration,
-      this.disableForegroundDecoration,
-      this.disabled});
-
   @override
   Widget build(BuildContext context) {
-    BorderRadius defaultRadius = BorderRadius.circular(NEUMORPHIC_DEFAULT_RADIUS);
+    BorderRadius deafultRadius =
+    BorderRadius.circular(NEUMORPHIC_DEFAULT_RADIUS);
     InnerShadowDecoration defaultForegroundDecoration = InnerShadowDecoration(
-        colors: pressed == true
-            ? [
-                CustomColors.containerInnerShadowTop,
-                CustomColors.containerInnerShadowBottom,
-              ]
-            : [
-                CustomColors.container,
-                CustomColors.container,
-              ],
-        borderRadius: borderRadius ?? defaultRadius);
+      colors: pressed == true
+          ? [
+        CustomColors.containerInnerShadowTop,
+        CustomColors.containerInnerShadowBottom
+      ]
+          : [
+        CustomColors.container,
+        CustomColors.container,
+      ],
+      borderRadius: borderRadius ?? deafultRadius,
+    );
+
     return Opacity(
       opacity: disabled == true ? .5 : 1,
       child: AnimatedContainer(
@@ -52,46 +63,38 @@ class NeumorphicContainer extends StatelessWidget {
         height: height,
         margin: margin,
         duration: const Duration(milliseconds: 150),
-        padding: pressed ?? const EdgeInsets.all(8),
-        foregroundDecoration:
-            disableForegroundDecoration != true ? foregroundDecoration ?? defaultForegroundDecoration : null,
+        padding: padding ?? const EdgeInsets.all(8.0),
+        foregroundDecoration: disableForegroundDecoration != true
+            ? foregroundDecoration ?? defaultForegroundDecoration
+            : null,
         decoration: decoration ??
             BoxDecoration(
-                borderRadius: borderRadius ?? defaultRadius,
-                border: border,
-                color: pressed == true ? CustomColors.containerPressed : color ?? CustomColors.container,
-            boxShadow: pressed == true ? null : [
-              BoxShadow(
-                blurRadius: 10,
-                offset: const Offset(-6, -6),
-                color: CustomColors.containerInnerShadowTop,
-              ),
-              BoxShadow(
-                blurRadius: 10,
-                offset: const Offset(-6, -6),
-                color: CustomColors.containerInnerShadowBottom,
-              )
-            ]),
+              borderRadius: borderRadius ?? deafultRadius,
+              border: border,
+              color: pressed == true
+                  ? CustomColors.containerPressed
+                  : color ?? CustomColors.container,
+              boxShadow: pressed == true
+                  ? null
+                  : [
+                BoxShadow(
+                  blurRadius: 10,
+                  offset: const Offset(-6, -6),
+                  color: CustomColors.containerShadowTop,
+                ),
+                BoxShadow(
+                  blurRadius: 10,
+                  offset: const Offset(6, 6),
+                  color: CustomColors.containerShadowBottom,
+                ),
+              ],
+            ),
         child: ClipRRect(
           clipBehavior: Clip.antiAlias,
-          borderRadius: borderRadius ?? defaultRadius,
+          borderRadius: borderRadius ?? deafultRadius,
           child: child,
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-

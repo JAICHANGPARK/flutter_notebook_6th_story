c563f8242ccf47f9e8adf45ba76068a71806c604
Dreamwalker <aristojeff@gmail.com>
2020-05-27 22:26:10 +0900

    Update neumorphic_icon_button.dart

--- lib/ep463_waching_machine/ui/neumorphic_icon_button.dart
+++ lib/ep463_waching_machine/ui/neumorphic_icon_button.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutternotebook6thstory/ep463_waching_machine/ui/neumorphic_button.dart';
 
+class NeumorphicIconButton extends StatelessWidget {
+  final Icon icon;
+  final EdgeInsetsGeometry margin;
+  final GestureTapCallback onTap;
+  final Color color;
+  final bool pressed;
+  final disabled;
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicButton(
+
+    );
+  }
+}

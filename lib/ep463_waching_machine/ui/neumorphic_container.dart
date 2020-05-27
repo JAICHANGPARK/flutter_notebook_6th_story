803990a5d3d650a87bbe4d8cae56c9fe2a9bc1ff
Dreamwalker <aristojeff@gmail.com>
2020-05-27 22:11:15 +0900

    Update neumorphic_container.dart

--- lib/ep463_waching_machine/ui/neumorphic_container.dart
+++ lib/ep463_waching_machine/ui/neumorphic_container.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class NeumorphicContainer extends StatelessWidget {
+  final bool pressed;
+  final double width;
+  final double height;
+  final  Widget child;
+  final EdgeInsetsGeometry margin;
+  final Color color;
   @override
   Widget build(BuildContext context) {
     return Container();

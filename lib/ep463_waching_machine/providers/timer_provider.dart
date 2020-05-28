7229f45836d1e11c00d6b3cfaf3f2ddd27bb306f
Dreamwalker <aristojeff@gmail.com>
2020-05-28 20:33:43 +0900

    Update timer_provider.dart

--- lib/ep463_waching_machine/providers/timer_provider.dart
+++ lib/ep463_waching_machine/providers/timer_provider.dart
@@ -4,5 +4,10 @@ import 'package:quiver/async.dart';
 class TimerProvider with ChangeNotifier{
   Duration _pausedRemaining;
   CountdownTimer _countdownTimer;
-  
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _countdownTimer?.cancel();
+    super.dispose();
+  }
 }

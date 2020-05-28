96edc628e1a529d498f7c5de184e5872e65fffd2
Dreamwalker <aristojeff@gmail.com>
2020-05-28 20:34:29 +0900

    Update timer_provider.dart

--- lib/ep463_waching_machine/providers/timer_provider.dart
+++ lib/ep463_waching_machine/providers/timer_provider.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:quiver/async.dart';
 
-class TimerProvider with ChangeNotifier{
+class TimerProvider with ChangeNotifier {
   Duration _pausedRemaining;
   CountdownTimer _countdownTimer;
+
   @override
   void dispose() {
     // TODO: implement dispose
     _countdownTimer?.cancel();
     super.dispose();
+  }
+
+  Duration get remaining {
+    if (_pausedRemaining != null) {
+      return _pausedRemaining;
+    }
+    return _countdownTimer?.isRunning == true ? _countdownTimer.remaining : Duration.zero;
   }
 }

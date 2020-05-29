c27b44e1eef7c7abfb178236075dd652c1534494
Dreamwalker <aristojeff@gmail.com>
2020-05-29 20:14:13 +0900

    Update main_provider.dart

--- lib/ep463_waching_machine/providers/main_provider.dart
+++ lib/ep463_waching_machine/providers/main_provider.dart
@@ -16,5 +16,36 @@ class MainProvider with ChangeNotifier{
       minutes: 32,
       color: Color.fromRGBO(61, 111, 252, 1),
     ),
+    ModeItemModel(
+      name: "Gentle",
+      minutes: 24,
+      color: Color.fromRGBO(50, 197, 253, 1),
+    ),
+    ModeItemModel(
+      name: "Fast",
+      minutes: 12,
+      color: Color.fromRGBO(253, 133, 253, 1),
+    ),
   ];
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

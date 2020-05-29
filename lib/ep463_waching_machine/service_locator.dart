6dc53aed7a34c4b0f8ab565bf71fff4c87fae802
Dreamwalker <aristojeff@gmail.com>
2020-05-29 20:24:09 +0900

    Update service_locator.dart

--- lib/ep463_waching_machine/service_locator.dart
+++ lib/ep463_waching_machine/service_locator.dart
@@ -1,3 +1,4 @@
+import 'package:flutternotebook6thstory/ep463_waching_machine/providers/main_provider.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/providers/settings_provider.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/providers/theme_provider.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/providers/timer_provider.dart';
@@ -12,6 +13,7 @@ class ServiceLocator {
     getIt.registerSingleton<TimerProvider>(TimerProvider());
     getIt.registerSingleton<WashingMachineController>(WashingMachineController(ballsCount: 16));
     getIt.registerSingleton<SettingsProvider>(SettingsProvider());
+    getIt.registerSingleton<MainProvider>(MainProvider());
   }
 
   static T get<T>() {

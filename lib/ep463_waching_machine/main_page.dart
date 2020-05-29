5cc4721bc2b0bf09db77d57ca35edff0cf94aa07
Dreamwalker <aristojeff@gmail.com>
2020-05-29 20:42:27 +0900

    Update main_page.dart

--- lib/ep463_waching_machine/main_page.dart
+++ lib/ep463_waching_machine/main_page.dart
@@ -236,6 +236,16 @@ class __IndicatorState extends State<_Indicator> with SingleTickerProviderStateM
     _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 700));
   }
 
+  void setupAnimation(){
+    
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(

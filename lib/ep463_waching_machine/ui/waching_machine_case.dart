fabf23ecd3dd887fa7a2e90b4e77f44e406d49ba
Dreamwalker <aristojeff@gmail.com>
2020-05-30 19:53:27 +0900

    Create waching_machine_case.dart

---
+++ lib/ep463_waching_machine/ui/waching_machine_case.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class WashingMachineCase extends StatefulWidget {
+  @override
+  _WashingMachineCaseState createState() => _WashingMachineCaseState();
+}
+
+class _WashingMachineCaseState extends State<WashingMachineCase> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

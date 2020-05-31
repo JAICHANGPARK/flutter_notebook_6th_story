3c3fe0ab264133c3eef07ee95b1029dd43dce661
Dreamwalker <aristojeff@gmail.com>
2020-05-31 19:02:13 +0900

    Update drum_physic_renderer.dart

--- lib/ep463_waching_machine/ui/drum_physic_renderer.dart
+++ lib/ep463_waching_machine/ui/drum_physic_renderer.dart
@@ -2,22 +2,30 @@ import 'dart:ui';
 
 import 'package:box2d_flame/box2d.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class DrumPhysicRenderer {
+  DrumPhysicRenderer({
+    @required this.ppm,
+  });
+
   final double ppm;
 
-  DrumPhysicRenderer({@required this.ppm});
-
   renderBody(Canvas canvas, Body body) {
-    if (body == null) return;
+    if (body == null) {
+      return;
+    }
+
     double angle = body.getAngle();
     Vector2 position = body.position * ppm;
     Color color = body.userData as Color;
+
     Matrix4 matrix = Matrix4.identity()
       ..leftTranslate(position.x, position.y)
       ..rotateZ(angle);
     canvas.save();
     canvas.transform(matrix.storage);
+
     for (Fixture f = body.getFixtureList(); f != null; f = f.getNext()) {
       if (f.getType() == ShapeType.CIRCLE) {
         _drawCircleShape(canvas, f.getShape(), color);
@@ -27,6 +35,7 @@ class DrumPhysicRenderer {
         _drawChainShape(canvas, f.getShape(), color);
       }
     }
+
     canvas.restore();
   }
 
@@ -43,16 +52,12 @@ class DrumPhysicRenderer {
     int vertexCount = polygon.getVertexCount();
     List<Offset> points = [];
     for (int i = 0; i < vertexCount; i++) {
-      Vector2 vector2 = polygon.getVertex(i) * ppm;
-      points.add(Offset(vector2.x, vector2.y));
+      Vector2 vertex = polygon.getVertex(i) * ppm;
+      points.add(Offset(vertex.x, vertex.y));
     }
+
     canvas.drawRect(
-        Rect.fromLTRB(
-          points[0].dx,
-          points[2].dy,
-          points[2].dx,
-          points[0].dy,
-        ),
+        Rect.fromLTRB(points[0].dx, points[2].dy, points[2].dx, points[0].dy),
         Paint()
           ..strokeWidth = 1
           ..style = PaintingStyle.fill
@@ -60,12 +65,14 @@ class DrumPhysicRenderer {
   }
 
   _drawChainShape(Canvas canvas, ChainShape chain, Color color) {
-    int vertexCount = chain.getVertexCount();
     List<Offset> points = [];
+    int vertexCount = chain.getVertexCount();
+
     for (int i = 0; i < vertexCount; i++) {
-      Vector2 vector2 = chain.getVertex(i) * ppm;
-      points.add(Offset(vector2.x, vector2.y));
+      Vector2 vertex = chain.getVertex(i) * ppm;
+      points.add(Offset(vertex.x, vertex.y));
     }
+
     canvas.drawPoints(
         PointMode.lines,
         points,

bdc4c50b568dfbb960fd2769abe8c9e30bbc2075
Dreamwalker <aristojeff@gmail.com>
2020-05-31 18:54:49 +0900

    Update washing_machine_drum.dart

--- lib/ep463_waching_machine/ui/washing_machine_drum.dart
+++ lib/ep463_waching_machine/ui/washing_machine_drum.dart
@@ -1,47 +1,29 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/ui/drum_phsic.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/ui/washing_machine_controller.dart';
 
 import 'drum_physic_renderer.dart';
 
-class WashingMachineDrum extends LeafRenderObjectWidget{
+class WashingMachineDrum extends LeafRenderObjectWidget {
   final WashingMachineController controller;
-
 
   WashingMachineDrum(this.controller);
 
   @override
   RenderObject createRenderObject(BuildContext context) {
     // TODO: implement createRenderObject
-    throw UnimplementedError();
+    return _WhirlpoolRenderObject()..controller = controller;
   }
-
 }
 
 class _WhirlpoolRenderObject extends RenderBox {
   DrumPhysicRenderer _physicRenderer = DrumPhysicRenderer(ppm: DrumPhysic.PPM);
   WashingMachineController _controller;
+
   WashingMachineController get controller => _controller;
   double _lastTimeStamp = 0.0;
 
+  set controller(WashingMachineController v) {
+    if (_controller == v) return;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

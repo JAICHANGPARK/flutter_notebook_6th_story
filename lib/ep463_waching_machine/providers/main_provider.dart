2978978236d2c7c43e46b41f438b487f582f383e
Dreamwalker <aristojeff@gmail.com>
2020-05-31 18:26:22 +0900

    Update main_provider.dart

--- lib/ep463_waching_machine/providers/main_provider.dart
+++ lib/ep463_waching_machine/providers/main_provider.dart
@@ -10,42 +10,93 @@ import 'package:flutternotebook6thstory/ep463_waching_machine/service_locator.da
 import 'package:flutternotebook6thstory/ep463_waching_machine/ui/washing_machine_controller.dart';
 
 class MainProvider with ChangeNotifier {
-  MainProvider() {
+  MainViewModel() {
     selectMode(nodes.first);
   }
 
   List<ModeItemModel> nodes = const [
     ModeItemModel(
-      name: "Standard",
+      name: 'Standard',
       minutes: 32,
       color: Color.fromRGBO(61, 111, 252, 1),
     ),
     ModeItemModel(
-      name: "Gentle",
+      name: 'Gentle',
       minutes: 24,
       color: Color.fromRGBO(50, 197, 253, 1),
     ),
     ModeItemModel(
-      name: "Fast",
+      name: 'Fast',
       minutes: 12,
-      color: Color.fromRGBO(253, 133, 253, 1),
+      color: Color.fromRGBO(253, 133, 53, 1),
     ),
   ];
 
-  double _waterValue = 680.0;
-  ModeStatus _modeStatus = ModeStatus.notStarted;
-  ModeItemModel _selectedMode;
-
   double get waterValue => _waterValue;
-
-  set waterValue(double d) {
-    _waterValue = d;
+  set waterValue(double value) {
+    _waterValue = value;
     notifyListeners();
   }
 
   ModeItemModel get selectedMode => _selectedMode;
-
   ModeStatus get modeStatus => _modeStatus;
+
+  double _waterValue = 600.0;
+  ModeItemModel _selectedMode;
+  ModeStatus _modeStatus = ModeStatus.notStarted;
+
+  runOrPause() {
+    if (selectedMode == null) {
+      return;
+    }
+
+    var timerVM = ServiceLocator.get<TimerProvider>();
+    var washingMachineController =
+    ServiceLocator.get<WashingMachineController>();
+
+    if (modeStatus == ModeStatus.running) {
+      _modeStatus = ModeStatus.paused;
+      timerVM.pause();
+      washingMachineController.setAngularVelocity(0, seconds: 1);
+    } else if (modeStatus == ModeStatus.paused) {
+      _modeStatus = ModeStatus.running;
+      washingMachineController.setAngularVelocity(-15, seconds: 7);
+      timerVM.resume();
+    } else if (modeStatus == ModeStatus.notStarted) {
+      _modeStatus = ModeStatus.running;
+
+      if (!washingMachineController.hasBalls()) {
+        washingMachineController.initializeBalls();
+      }
+
+      timerVM.start(Duration(minutes: selectedMode.minutes));
+
+      Timer.periodic(
+          Duration(seconds: !washingMachineController.hasBalls() ? 2 : 0),
+              (timer) {
+            timer.cancel();
+            if (modeStatus != ModeStatus.running) {
+              return;
+            }
+
+            washingMachineController.setAngularVelocity(-15, seconds: 7);
+          });
+    }
+
+    notifyListeners();
+  }
+
+  stop() {
+    var washingMachineController =
+    ServiceLocator.get<WashingMachineController>();
+    var timerVM = ServiceLocator.get<TimerProvider>();
+
+    washingMachineController.setAngularVelocity(0, seconds: 3);
+    _modeStatus = ModeStatus.notStarted;
+    timerVM.reset(callNotifyListeners: true);
+
+    notifyListeners();
+  }
 
   selectMode(ModeItemModel mode) {
     if (mode == _selectedMode) {
@@ -56,48 +107,13 @@ class MainProvider with ChangeNotifier {
     }
     _selectedMode = mode;
     _modeStatus = ModeStatus.notStarted;
+
     var timerVM = ServiceLocator.get<TimerProvider>();
     timerVM.reset(callNotifyListeners: true);
     notifyListeners();
+
     int sign = Random().nextBool() ? 1 : -1;
-    ServiceLocator.get<WashingMachineController>().setAngularVelocity(9.0 * sign, stopAtEnd: true, seconds: 0.6);
-  }
-
-  runOrPause() {
-    if (selectedMode == null) return;
-
-    var wachingMachineController = ServiceLocator.get<WashingMachineController>();
-    var timerProvider = ServiceLocator.get<TimerProvider>();
-
-    if (modeStatus == ModeStatus.running) {
-      _modeStatus = ModeStatus.paused;
-      timerProvider.pause();
-      wachingMachineController.setAngularVelocity(0, seconds: 1);
-    } else if (modeStatus == ModeStatus.paused) {
-      _modeStatus = ModeStatus.running;
-      wachingMachineController.setAngularVelocity(-15, seconds: 7);
-      timerProvider.resume();
-    } else if (modeStatus == ModeStatus.notStarted) {
-      _modeStatus = ModeStatus.running;
-      if (wachingMachineController.hasBalls()) {
-        wachingMachineController.initializeBalls();
-      }
-      timerProvider.start(Duration(minutes: selectedMode.minutes));
-      Timer.periodic(Duration(seconds: !wachingMachineController.hasBalls() ? 2 : 0), (timer) {
-        timer.cancel();
-        if (modeStatus != ModeStatus.running) return;
-        wachingMachineController.setAngularVelocity(-15, seconds: 7);
-      });
-    }
-    notifyListeners();
-  }
-
-  stop() {
-    var wachingMachineController = ServiceLocator.get<WashingMachineController>();
-    var timerProvider = ServiceLocator.get<TimerProvider>();
-    wachingMachineController.setAngularVelocity(0, seconds: 3);
-    _modeStatus = ModeStatus.notStarted;
-    timerProvider.reset(callNotifyListeners: true);
-    notifyListeners();
+    ServiceLocator.get<WashingMachineController>()
+        .setAngularVelocity(9.0 * sign, stopAtEnd: true, seconds: 0.6);
   }
 }

c7dbf1d4b5d79793479592e068a4368bd15644e4
Dreamwalker <aristojeff@gmail.com>
2020-05-31 18:41:32 +0900

    Update drum_physic_renderer.dart

--- lib/ep463_waching_machine/ui/drum_physic_renderer.dart
+++ lib/ep463_waching_machine/ui/drum_physic_renderer.dart
@@ -1,7 +1,16 @@
+import 'package:box2d_flame/box2d.dart';
 import 'package:flutter/material.dart';
 
 class DrumPhysicRenderer{
   final double ppm;
 
   DrumPhysicRenderer({@required this.ppm});
+
+  renderBody(Canvas canvas, Body body){
+    if(body == null) return;
+    double angle = body.getAngle();
+    Vector2 position = body.position * ppm;
+    Color color = body.userData as Color;
+
+  }
 }

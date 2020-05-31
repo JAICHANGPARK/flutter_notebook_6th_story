182746aef4572a1b1bbca056df464605a54cf5b1
Dreamwalker <aristojeff@gmail.com>
2020-05-31 18:25:10 +0900

    Update timer_provider.dart

--- lib/ep463_waching_machine/providers/timer_provider.dart
+++ lib/ep463_waching_machine/providers/timer_provider.dart
@@ -2,57 +2,62 @@ import 'package:flutter/foundation.dart';
 import 'package:quiver/async.dart';
 
 class TimerProvider with ChangeNotifier {
-  Duration _pausedRemaining;
-  CountdownTimer _countdownTimer;
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _countdownTimer?.cancel();
-    super.dispose();
-  }
-
   Duration get remaining {
     if (_pausedRemaining != null) {
       return _pausedRemaining;
     }
-    return _countdownTimer?.isRunning == true ? _countdownTimer.remaining : Duration.zero;
+
+    return _countdownTimer?.isRunning == true
+        ? _countdownTimer?.remaining
+        : Duration.zero;
   }
 
-  String get remainingString {
+  String get remainigString {
     int totalSeconds = remaining.inSeconds;
     int minutes = (totalSeconds / 60).floor();
-    int second = totalSeconds % 60;
-    String minutesString = minutes > 9 ? minutes.toString() : "0$minutes";
-    String secondsString = second > 9 ? second.toString() : "0$second";
+    int seconds = totalSeconds % 60;
+
+    String minutesString = minutes > 9 ? minutes.toString() : '0$minutes';
+    String secondsString = seconds > 9 ? seconds.toString() : '0$seconds';
+
     return '$minutesString:$secondsString';
+  }
+
+  CountdownTimer _countdownTimer;
+  Duration _pausedRemaining;
+
+  @override
+  void dispose() {
+    _countdownTimer?.cancel();
+    super.dispose();
   }
 
   start(Duration duration) {
     if (duration == null) {
       return;
     }
-    reset(callNofityListeners: true);
+    reset(callNotifyListeners: true);
+
     _countdownTimer = CountdownTimer(duration, Duration(seconds: 1));
-    _countdownTimer.listen((event) => notifyListeners());
+    _countdownTimer.listen((timer) => notifyListeners());
   }
 
   resume() {
     start(_pausedRemaining);
   }
 
+  reset({bool callNotifyListeners}) {
+    _pausedRemaining = null;
+    _countdownTimer?.cancel();
+
+    if (callNotifyListeners == true) {
+      notifyListeners();
+    }
+  }
+
   pause() {
     _pausedRemaining = remaining;
     _countdownTimer?.cancel();
     notifyListeners();
-  }
-
-  reset({bool callNofityListeners}) {
-    _pausedRemaining = null;
-    _countdownTimer?.cancel();
-
-    if (callNofityListeners == true) {
-      notifyListeners();
-    }
   }
 }

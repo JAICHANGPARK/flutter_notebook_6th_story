53eaf6d38d9a4f90312e376adff5040ecd2082aa
Dreamwalker <aristojeff@gmail.com>
2020-05-31 18:44:00 +0900

    Update drum_physic_renderer.dart

--- lib/ep463_waching_machine/ui/drum_physic_renderer.dart
+++ lib/ep463_waching_machine/ui/drum_physic_renderer.dart
@@ -1,19 +1,49 @@
 import 'package:box2d_flame/box2d.dart';
 import 'package:flutter/material.dart';
 
-class DrumPhysicRenderer{
+class DrumPhysicRenderer {
   final double ppm;
 
   DrumPhysicRenderer({@required this.ppm});
 
-  renderBody(Canvas canvas, Body body){
-    if(body == null) return;
+  renderBody(Canvas canvas, Body body) {
+    if (body == null) return;
     double angle = body.getAngle();
     Vector2 position = body.position * ppm;
     Color color = body.userData as Color;
-    Matrix4 matrix = Matrix4.identity()..leftTranslate(position.x, position.y)
-    ..rotateZ(angle);
+    Matrix4 matrix = Matrix4.identity()
+      ..leftTranslate(position.x, position.y)
+      ..rotateZ(angle);
     canvas.save();
     canvas.transform(matrix.storage);
+    for(Fixture f = body.getFixtureList(); f != null; f = f.getNext()){
+      if(f.getType() == ShapeType.CIRCLE){
+
+      }else if(f.getType() == ShapeType.POLYGON){
+
+      }else if(f.getType() == ShapeType.CHAIN){
+
+      }
+    }
+    canvas.restore();
   }
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+

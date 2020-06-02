cb1b5378ad7a475f6223212ae044df78d4cb2b45
Dreamwalker <aristojeff@gmail.com>
2020-06-02 20:04:07 +0900

    Update water_drawer.dart

--- lib/ep463_waching_machine/water_drawer.dart
+++ lib/ep463_waching_machine/water_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/providers/main_provider.dart';
+import 'package:flutternotebook6thstory/ep463_waching_machine/ui/neumorphic_icon_button.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/utils/colors.dart';
 import 'package:provider/provider.dart';
 
@@ -13,7 +14,13 @@ class WaterDrawer extends StatelessWidget {
           color: CustomColors.primaryColor,
           child: SafeArea(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                NeumorphicIconButton(
+                  icon: Icon(Icons.arrow_back_ios,
+                  size: 18, color: CustomColors.icon,),
+
+                )
 
               ],
             ),

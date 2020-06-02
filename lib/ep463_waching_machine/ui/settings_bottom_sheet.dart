a295003ea8cb367dfa81f658fa8f5536cc244f0b
Dreamwalker <aristojeff@gmail.com>
2020-06-02 19:53:15 +0900

    Create settings_bottom_sheet.dart

---
+++ lib/ep463_waching_machine/ui/settings_bottom_sheet.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class SettingsBottomSheet extends StatefulWidget {
+  @override
+  _SettingsBottomSheetState createState() => _SettingsBottomSheetState();
+}
+
+class _SettingsBottomSheetState extends State<SettingsBottomSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

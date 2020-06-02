73e82ccb161d4d8010d672123b3cfa6ad0d8d041
Dreamwalker <aristojeff@gmail.com>
2020-06-02 20:02:37 +0900

    Update water_drawer.dart

--- lib/ep463_waching_machine/water_drawer.dart
+++ lib/ep463_waching_machine/water_drawer.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutternotebook6thstory/ep463_waching_machine/providers/main_provider.dart';
+import 'package:flutternotebook6thstory/ep463_waching_machine/utils/colors.dart';
+import 'package:provider/provider.dart';
 
 
 class WaterDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer<MainProvider>(
+      builder: (context, value, _){
+        return Container(
+          color: CustomColors.primaryColor,
+        );
+      },
+    );
   }
 }

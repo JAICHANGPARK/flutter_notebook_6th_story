2b44967ad2de86607a4acfb0b81984ed6a7a9390
Dreamwalker <aristojeff@gmail.com>
2020-06-03 20:22:18 +0900

    Update water_slider.dart

---
+++ lib/ep463_waching_machine/ui/water_slider.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+typedef ValueChangeCallback = void Function(double value);
+
+class WaterSlider extends StatefulWidget {
+  @override
+  _WaterSliderState createState() => _WaterSliderState();
+}
+
+class _WaterSliderState extends State<WaterSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

eb038bec5b4c6022734a44a9fee52693f2c9536e
Dreamwalker <aristojeff@gmail.com>
2020-06-03 20:16:33 +0900

    Update water_drawer.dart

--- lib/ep463_waching_machine/water_drawer.dart
+++ lib/ep463_waching_machine/water_drawer.dart
@@ -1,72 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:flutternotebook6thstory/ep463_waching_machine/providers/main_provider.dart';
-import 'package:flutternotebook6thstory/ep463_waching_machine/ui/neumorphic_icon_button.dart';
-import 'package:flutternotebook6thstory/ep463_waching_machine/utils/colors.dart';
-import 'package:flutternotebook6thstory/ep463_waching_machine/utils/consts.dart';
+import 'package:flutter_whirlpool/screens/water_drawer/water_slider.dart';
+import 'package:flutter_whirlpool/shared/colors.dart';
+import 'package:flutter_whirlpool/shared/consts.dart';
+import 'package:flutter_whirlpool/shared/widgets.dart';
+import 'package:flutter_whirlpool/view_models/main_view_model.dart';
 import 'package:provider/provider.dart';
 
 class WaterDrawer extends StatelessWidget {
+  const WaterDrawer({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<MainProvider>(
-      builder: (context, value, _) {
+    return Consumer<MainViewModel>(
+      builder: (context, viewModel, _) {
         return Container(
           color: CustomColors.primaryColor,
           child: SafeArea(
             child: Padding(
-              padding: const EdgeInsets.only(left: GLOBAL_EDGE_MARGIN_VALUE, top: DRAWER_BUTTON_MARGIN_TOP),
+              padding: EdgeInsets.only(
+                left: GLOBAL_EDGE_MARGIN_VALUE,
+                top: DRAWER_BUTTON_MARGIN_TOP,
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
+                children: <Widget>[
                   NeumorphicIconButton(
                     icon: Icon(
                       Icons.arrow_back_ios,
                       size: 18,
                       color: CustomColors.icon,
                     ),
-                    onTap: () => Navigator.of(context).pop(),
+                    onTap: () => Navigator.pop(context),
                   ),
-                  SizedBox(
-                    height: 35,
-                  ),
+                  SizedBox(height: 35),
                   Text(
-                    "Choose water",
+                    'Choose water',
                     style: TextStyle(
                       fontSize: 26,
                       color: CustomColors.primaryTextColor,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
-                  SizedBox(
-                    height: 5,
-                  ),
+                  SizedBox(height: 3),
                   Text(
-                    "Please save choice",
+                    'Please save choice',
                     style: TextStyle(
                       fontSize: 14,
                       color: CustomColors.primaryTextColor,
                       fontWeight: FontWeight.w400,
                     ),
                   ),
-                  SizedBox(
-                    height: 40,
-                  ),
+                  SizedBox(height: 40),
                   Expanded(
-                    child: Placeholder(),
+                    child: WaterSlider(
+                      minValue: 200,
+                      maxValue: 1200,
+                      initValue: viewModel.waterValue,
+                      onValueChanged: (newValue) =>
+                      viewModel.waterValue = newValue,
+                    ),
                   ),
-                  SizedBox(
-                    height: 80,
-                  ),
-                  Text.rich(TextSpan(
-                      text: "Current   ",
-                      style: TextStyle(color: CustomColors.primaryTextColor, fontSize: 18, fontWeight: FontWeight.w300),
-                      children: [
+                  SizedBox(height: 80),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Current  ',
+                      style: TextStyle(
+                        color: CustomColors.primaryTextColor,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 18,
+                      ),
+                      children: <TextSpan>[
                         TextSpan(
-                            text: value.waterValue.toStringAsFixed(0), style: TextStyle(fontWeight: FontWeight.w700))
-                      ])),
-                  SizedBox(
-                    height: 40,
+                          text: viewModel.waterValue.toStringAsFixed(0),
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
                   ),
+                  SizedBox(height: 40),
                 ],
               ),
             ),

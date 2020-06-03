349c27da677a7bc6797ff9e0235709f3fbc2877f
Dreamwalker <aristojeff@gmail.com>
2020-06-03 20:24:59 +0900

    Update water_slider.dart

--- lib/ep463_waching_machine/ui/water_slider.dart
+++ lib/ep463_waching_machine/ui/water_slider.dart
@@ -3,23 +3,17 @@ import 'package:flutter/material.dart';
 typedef ValueChangeCallback = void Function(double value);
 
 class WaterSlider extends StatefulWidget {
-
   final double minValue;
   final double maxValue;
   final double initValue;
   final ValueChangeCallback onValueChanged;
 
-  WaterSlider({
-    Key key,
-    @required this.minValue,
-    @required this.maxValue,
-    this.initValue, this.onValueChanged}) : super(key: key) {
-      assert(minValue < maxValue);
-      assert(minValue >= 0);
-      assert(maxValue >= 0);
+  WaterSlider({Key key, @required this.minValue, @required this.maxValue, this.initValue, this.onValueChanged})
+      : super(key: key) {
+    assert(minValue < maxValue);
+    assert(minValue >= 0);
+    assert(maxValue >= 0);
   }
-
-
 
   @override
   _WaterSliderState createState() => _WaterSliderState();
@@ -31,8 +25,6 @@ class _WaterSliderState extends State<WaterSlider> {
     return Container();
   }
 }
-
-
 
 
 

52efd5669139926630cf1d5b5b6567ff06b669e1
Dreamwalker <aristojeff@gmail.com>
2020-06-03 20:36:27 +0900

    Update water_slider.dart

--- lib/ep463_waching_machine/ui/water_slider.dart
+++ lib/ep463_waching_machine/ui/water_slider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutternotebook6thstory/ep463_waching_machine/ui/inner_shadow_decorator.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/ui/neumorphic_container.dart';
 import 'package:flutternotebook6thstory/ep463_waching_machine/utils/colors.dart';
 
@@ -45,32 +46,63 @@ class _WaterSliderState extends State<WaterSlider> with SingleTickerProviderStat
   @override
   Widget build(BuildContext context) {
     BorderRadius borderRadius = BorderRadius.circular(50);
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-      return Row(
-        children: [
-          NeumorphicContainer(
-            width: 85,
-            color: CustomColors.containerPressed,
-            borderRadius: borderRadius,
-          )
-        ],
-      );
-    },
-
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Row(
+          children: [
+            NeumorphicContainer(
+              width: 85,
+              color: CustomColors.containerPressed,
+              borderRadius: borderRadius,
+              disableForegroundDecoration: true,
+              padding: EdgeInsets.zero,
+              child: Container(
+                decoration: InnerShadowDecoration(
+                  colors: [
+                    CustomColors.containerInnerShadowTop,
+                    CustomColors.containerInnerShadowBottom,
+                  ],
+                  borderRadius: borderRadius,
+                ),
+                child: Stack(
+                  children: [],
+                ),
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
 
+class _WaterSlide extends StatefulWidget {
+  final double height;
+  final double min;
+  final double max;
+  final double topOffset;
+  final double bottomOffset;
+  final double initValue;
+  final Animation<double> controller;
+  final ValueChangeCallback onValueChanged;
 
+  _WaterSlide(
+      {this.height,
+      this.min,
+      this.max,
+      this.topOffset = 0.0,
+      this.bottomOffset = 0.0,
+      this.initValue,
+      this.controller,
+      this.onValueChanged});
 
+  @override
+  __WaterSlideState createState() => __WaterSlideState();
+}
 
-
-
-
-
-
-
-
-
-
-
+class __WaterSlideState extends State<_WaterSlide> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

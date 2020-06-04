b820e9ea8dbf5ed0e24e46c1fe131516b37c3cdf
Dreamwalker <aristojeff@gmail.com>
2020-06-04 20:03:22 +0900

    Update wave_container.dart

--- lib/ep463_waching_machine/ui/wave_container.dart
+++ lib/ep463_waching_machine/ui/wave_container.dart
@@ -10,17 +10,17 @@ class WaveContainer extends StatefulWidget {
   final Duration duration;
   final int sinWidthFraction;
 
-  WaveContainer({this.size, this.offset, this.color, this.duration
-    = const Duration(seconds: 4), this.sinWidthFraction = 3});
+  WaveContainer(
+      {this.size, this.offset, this.color, this.duration = const Duration(seconds: 4), this.sinWidthFraction = 3});
 
   @override
   _WaveContainerState createState() => _WaveContainerState();
 }
 
-class _WaveContainerState extends State<WaveContainer> with
-TickerProviderStateMixin{
+class _WaveContainerState extends State<WaveContainer> with TickerProviderStateMixin {
   AnimationController animationController;
   List<Offset> animationListener = [];
+
   @override
   void initState() {
     // TODO: implement initState
@@ -29,47 +29,22 @@ TickerProviderStateMixin{
     animationController = AnimationController(vsync: this, duration: widget.duration);
     animationController.addListener(() {
       animationListener.clear();
-      for(double i = -2- widget.offset.dx;
-      i <= widget.size.width.toInt() + 2;
-      i++){
+      for (double i = -2 - widget.offset.dx; i <= widget.size.width.toInt() + 2; i++) {
         print(i);
-        animationListener.add(Offset(
-            i.toDouble() + widget.offset.dx,
-            sin(((animationController.value * 360 - i) %
-                360 *
-                Vector.degrees2Radians) *
-                widget.sinWidthFraction) *
-                8 +
-                10 +
-                widget.offset.dy));
-       
+        animationListener.add(
+          Offset(
+              i.toDouble() + widget.offset.dx,
+              sin(((animationController.value * 360 - i) % 360 * Vector.degrees2Radians) * widget.sinWidthFraction) *
+                      8 +
+                  10 +
+                  widget.offset.dy),
+        );
       }
     });
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

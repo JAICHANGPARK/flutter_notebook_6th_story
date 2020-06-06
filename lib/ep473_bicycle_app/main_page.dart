d772d5e56e193a0014b202651a15d2fbbf9364f1
Dreamwalker <aristojeff@gmail.com>
2020-06-06 19:00:33 +0900

    Update main_page.dart

--- lib/ep473_bicycle_app/main_page.dart
+++ lib/ep473_bicycle_app/main_page.dart
@@ -113,13 +113,20 @@ class _BicycleRentalMainPageState extends State<BicycleRentalMainPage> {
         },
         items: [
           BottomNavyBarItem(icon: Icon(Icons.home), title: Text("Home"), activeColor: bicycleAppColor),
-          BottomNavyBarItem(icon: Icon(Icons.motorcycle), title: Text("cycle"), inactiveColor: Colors.black),
-          BottomNavyBarItem(icon: Icon(Icons.email), title: Text("email"), inactiveColor: Colors.black),
-          BottomNavyBarItem(icon: Icon(Icons.multiline_chart), title: Text("Chart"), inactiveColor: Colors.black),
+          BottomNavyBarItem(
+              icon: Icon(Icons.motorcycle),
+              title: Text("cycle"),
+              inactiveColor: Colors.black,
+              activeColor: bicycleAppColor),
+          BottomNavyBarItem(
+              icon: Icon(Icons.email), title: Text("email"), inactiveColor: Colors.black, activeColor: bicycleAppColor),
+          BottomNavyBarItem(
+              icon: Icon(Icons.multiline_chart),
+              title: Text("Chart"),
+              inactiveColor: Colors.black,
+              activeColor: bicycleAppColor),
         ],
       ),
     );
   }
 }
-
-

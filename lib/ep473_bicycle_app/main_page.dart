3e98a19a00d5609ad6443111d23cbeb89aea0867
Dreamwalker <aristojeff@gmail.com>
2020-06-06 18:59:47 +0900

    Update main_page.dart

--- lib/ep473_bicycle_app/main_page.dart
+++ lib/ep473_bicycle_app/main_page.dart
@@ -114,8 +114,8 @@ class _BicycleRentalMainPageState extends State<BicycleRentalMainPage> {
         items: [
           BottomNavyBarItem(icon: Icon(Icons.home), title: Text("Home"), activeColor: bicycleAppColor),
           BottomNavyBarItem(icon: Icon(Icons.motorcycle), title: Text("cycle")),
-          BottomNavyBarItem(icon: Icon(Icons.home), title: Text("Home")),
-          BottomNavyBarItem(icon: Icon(Icons.multiline_chart), title: Text("Home")),
+          BottomNavyBarItem(icon: Icon(Icons.email), title: Text("email")),
+          BottomNavyBarItem(icon: Icon(Icons.multiline_chart), title: Text("Chart")),
         ],
       ),
     );

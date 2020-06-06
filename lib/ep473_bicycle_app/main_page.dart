e040669c77270017131dc13c26542e02f28fdc7e
Dreamwalker <aristojeff@gmail.com>
2020-06-06 19:11:09 +0900

    Update main_page.dart

--- lib/ep473_bicycle_app/main_page.dart
+++ lib/ep473_bicycle_app/main_page.dart
@@ -27,177 +27,179 @@ class _BicycleRentalMainPageState extends State<BicycleRentalMainPage> {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 220,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 16,
-                    top: 24,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text.rich(TextSpan(children: [
-                            TextSpan(text: "Hi, ", style: TextStyle(color: fontSecondColor, fontSize: 18)),
-                            TextSpan(text: "Dreamwalker", style: TextStyle(color: fontMainColor, fontSize: 18)),
-                          ])),
-                          SizedBox(
-                            height: 24,
-                          ),
-                          Text.rich(TextSpan(children: [
-                            TextSpan(text: "Select ", style: TextStyle(color: fontSecondColor, fontSize: 22)),
-                            TextSpan(
-                                text: "Bicycle",
-                                style: TextStyle(color: fontMainColor, fontSize: 22, fontWeight: FontWeight.bold)),
-                          ])),
-                          Text.rich(TextSpan(children: [
-                            TextSpan(
-                                text: "To Riding ",
-                                style: TextStyle(color: fontMainColor, fontSize: 22, fontWeight: FontWeight.bold)),
-                            TextSpan(text: "Now.", style: TextStyle(color: fontSecondColor, fontSize: 22)),
-                          ])),
-                          SizedBox(
-                            height: 24,
-                          ),
-                          Container(
-                              height: 48,
-                              width: MediaQuery.of(context).size.width - 32,
-                              decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(8)),
-                              padding: EdgeInsets.symmetric(horizontal: 16),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(Icons.search),
-                                  Expanded(
-                                      child: TextField(
-                                    decoration: InputDecoration(
-                                        border: InputBorder.none, hintText: "Where do you want to go biking?"),
-                                  )),
-                                  Container(
-                                    height: 38,
-                                    width: 38,
-                                    decoration:
-                                        BoxDecoration(color: bicycleAppColor, borderRadius: BorderRadius.circular(8)),
-                                    child: Center(
-                                      child: Icon(Icons.map),
-                                    ),
-                                  )
-                                ],
-                              ))
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    right: -2,
-                    top: -2,
-                    child: Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                          border: Border.all(color: bicycleAppColor), borderRadius: BorderRadius.circular(10)),
-                      child: Center(
-                        child: Container(
-                          height: 38,
-                          width: 38,
-                          decoration: BoxDecoration(
-                              border: Border.all(color: bicycleAppColor),
-                              borderRadius: BorderRadius.circular(8),
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                    color: bicycleAppColor.withOpacity(0.2),
-                                    blurRadius: 4,
-                                    spreadRadius: -4,
-                                    offset: Offset(0, 16))
-                              ]),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 16,
+                      top: 24,
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text.rich(TextSpan(children: [
+                              TextSpan(text: "Hi, ", style: TextStyle(color: fontSecondColor, fontSize: 18)),
+                              TextSpan(text: "Dreamwalker", style: TextStyle(color: fontMainColor, fontSize: 18)),
+                            ])),
+                            SizedBox(
+                              height: 24,
+                            ),
+                            Text.rich(TextSpan(children: [
+                              TextSpan(text: "Select ", style: TextStyle(color: fontSecondColor, fontSize: 22)),
+                              TextSpan(
+                                  text: "Bicycle",
+                                  style: TextStyle(color: fontMainColor, fontSize: 22, fontWeight: FontWeight.bold)),
+                            ])),
+                            Text.rich(TextSpan(children: [
+                              TextSpan(
+                                  text: "To Riding ",
+                                  style: TextStyle(color: fontMainColor, fontSize: 22, fontWeight: FontWeight.bold)),
+                              TextSpan(text: "Now.", style: TextStyle(color: fontSecondColor, fontSize: 22)),
+                            ])),
+                            SizedBox(
+                              height: 24,
+                            ),
+                            Container(
+                                height: 48,
+                                width: MediaQuery.of(context).size.width - 32,
+                                decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(8)),
+                                padding: EdgeInsets.symmetric(horizontal: 16),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.search),
+                                    Expanded(
+                                        child: TextField(
+                                      decoration: InputDecoration(
+                                          border: InputBorder.none, hintText: "Where do you want to go biking?"),
+                                    )),
+                                    Container(
+                                      height: 38,
+                                      width: 38,
+                                      decoration:
+                                          BoxDecoration(color: bicycleAppColor, borderRadius: BorderRadius.circular(8)),
+                                      child: Center(
+                                        child: Icon(Icons.map),
+                                      ),
+                                    )
+                                  ],
+                                ))
+                          ],
                         ),
                       ),
                     ),
+                    Positioned(
+                      right: -2,
+                      top: -2,
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: bicycleAppColor), borderRadius: BorderRadius.circular(10)),
+                        child: Center(
+                          child: Container(
+                            height: 38,
+                            width: 38,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: bicycleAppColor),
+                                borderRadius: BorderRadius.circular(8),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: bicycleAppColor.withOpacity(0.2),
+                                      blurRadius: 4,
+                                      spreadRadius: -4,
+                                      offset: Offset(0, 16))
+                                ]),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 64,
+                    padding: EdgeInsets.only(left: 16),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 70,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                bottom: 0,
+                                child: CircleAvatar(
+                                  radius: 12,
+                                  backgroundColor: bicycleAppColor,
+                                ),
+                              ),
+                              Positioned(
+                                  left: 0,
+                                  top: 0,
+                                  bottom: 0,
+                                  child: Center(
+                                    child: Text(
+                                      "Newest",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 17,
+                                      ),
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          width: 70,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                  left: 0,
+                                  top: 0,
+                                  bottom: 0,
+                                  child: Center(
+                                    child: Text(
+                                      "Popular",
+                                      style: TextStyle(fontSize: 17,
+                                      fontWeight: FontWeight.w300),
+
+                                    ),
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 160,
+                    child: Placeholder(),
+                  ),
+                  Container(
+                    height: 64,
+                    child: Placeholder(),
+                  ),
+                  Container(
+                    height: 120,
+                    child: Placeholder(),
+                  ),
+                  Container(
+                    height: 120,
+                    child: Placeholder(),
                   )
                 ],
-              ),
-            ),
-            Column(
-              children: [
-                Container(
-                  height: 64,
-                  padding: EdgeInsets.only(left: 16),
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 70,
-                        child: Stack(
-                          children: [
-                            Positioned(
-                              left: 0,
-                              top: 0,
-                              bottom: 0,
-                              child: CircleAvatar(
-                                radius: 12,
-                                backgroundColor: bicycleAppColor,
-                              ),
-                            ),
-                            Positioned(
-                                left: 0,
-                                top: 0,
-                                bottom: 0,
-                                child: Center(
-                                  child: Text(
-                                    "Newest",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 17,
-                                    ),
-                                  ),
-                                )),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 70,
-                        child: Stack(
-                          children: [
-                            Positioned(
-                                left: 0,
-                                top: 0,
-                                bottom: 0,
-                                child: Center(
-                                  child: Text(
-                                    "Popular",
-                                    style: TextStyle(fontSize: 17,
-                                    fontWeight: FontWeight.w300),
-
-                                  ),
-                                )),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 160,
-                  child: Placeholder(),
-                ),
-                Container(
-                  height: 64,
-                  child: Placeholder(),
-                ),
-                Container(
-                  height: 120,
-                  child: Placeholder(),
-                ),
-                Container(
-                  height: 120,
-                  child: Placeholder(),
-                )
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavyBar(

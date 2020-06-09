c4f175af85d7d938e87a391e23c50cecf557921e
Dreamwalker <aristojeff@gmail.com>
2020-06-09 19:36:42 +0900

    Update dog_lovers_main_page.dart

--- lib/ep477_dog_lovers_app/dog_lovers_main_page.dart
+++ lib/ep477_dog_lovers_app/dog_lovers_main_page.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class DogLoversMainPage extends StatefulWidget {
   @override
   _DogLoversMainPageState createState() => _DogLoversMainPageState();
 }
 
-class _DogLoversMainPageState extends State<DogLoversMainPage> {
+class _DogLoversMainPageState extends State<DogLoversMainPage>
+with TickerProviderStateMixin{
+  AnimationController animationController;
+  Animation<double> _animation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+
     );
   }
 }

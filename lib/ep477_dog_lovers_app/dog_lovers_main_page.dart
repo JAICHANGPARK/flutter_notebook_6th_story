a361794208e8983734693067589d3bcdb4bf07e3
Dreamwalker <aristojeff@gmail.com>
2020-06-09 19:34:11 +0900

    Update dog_lovers_main_page.dart

--- lib/ep477_dog_lovers_app/dog_lovers_main_page.dart
+++ lib/ep477_dog_lovers_app/dog_lovers_main_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class DogLoversMainPage extends StatefulWidget {
+  @override
+  _DogLoversMainPageState createState() => _DogLoversMainPageState();
+}
+
+class _DogLoversMainPageState extends State<DogLoversMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

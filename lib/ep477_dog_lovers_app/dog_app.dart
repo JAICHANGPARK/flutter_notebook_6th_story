e581da7520ba21a1159cfe6615383192d9ff8750
Dreamwalker <aristojeff@gmail.com>
2020-06-09 19:41:45 +0900

    Update dog_app.dart

--- lib/ep477_dog_lovers_app/dog_app.dart
+++ lib/ep477_dog_lovers_app/dog_app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutternotebook6thstory/ep477_dog_lovers_app/dog_lovers_main_page.dart';
 
 class DogLoversApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DogLoverMainPage(),
+      home: DogLoversMainPage(),
     );
   }
 }

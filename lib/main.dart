3ec1e84f09e68c96ca6614075c12a44fdc5494c2
Dreamwalker <aristojeff@gmail.com>
2020-06-12 19:38:52 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ep477_dog_lovers_app/dog_app.dart';
+import 'ep480_passhub_kiosk_app/kiosk_main_page.dart';
 
 void main() {
-  runApp(DogLoversApp());
+  runApp(KioskApp());
 }

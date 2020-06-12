6c28a9692c5af8c80fb2aed0b49d84c5aa21da8a
Dreamwalker <aristojeff@gmail.com>
2020-06-12 19:58:32 +0900

    Update kiosk_detail_page.dart

--- lib/ep480_passhub_kiosk_app/kiosk_detail_page.dart
+++ lib/ep480_passhub_kiosk_app/kiosk_detail_page.dart
@@ -42,37 +42,29 @@ class _KioskDetailPageState extends State<KioskDetailPage> {
                             Expanded(
                               flex: 8,
                               child: Container(
-                                margin: EdgeInsets.symmetric(
-                                  vertical: 8,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: Colors.blueGrey[100],
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    icon: Icon(Icons.style)
-
+                                  margin: EdgeInsets.symmetric(
+                                    vertical: 8,
                                   ),
-                                )
-                              ),
+                                  decoration: BoxDecoration(
+                                    color: Colors.blueGrey[100],
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none, icon: Icon(Icons.style), hintText: "123456789"),
+                                  )),
                             ),
                             Expanded(
                               flex: 2,
                               child: Container(
-                                margin: EdgeInsets.only(
-                                  left: 24,
-                                  right: 0,
-                                  top: 8,
-                                  bottom: 8
-                                ),
+                                margin: EdgeInsets.only(left: 24, right: 0, top: 8, bottom: 8),
                                 decoration: BoxDecoration(
-                                  color: Colors.lightBlueAccent,
-                                  borderRadius: BorderRadius.circular(8)
-                                ),
+                                    color: Colors.lightBlueAccent, borderRadius: BorderRadius.circular(8)),
                                 child: Center(
-                                  child: Icon(Icons.arrow_forward, color: Colors.white,),
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.white,
+                                  ),
                                 ),
                               ),
                             )

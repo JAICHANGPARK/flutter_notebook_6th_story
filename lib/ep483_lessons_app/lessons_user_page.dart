98818e636e0bad4a76206fbb8f4b5bf5b88d57a2
Dreamwalker <aristojeff@gmail.com>
2020-06-15 19:29:44 +0900

    Update lessons_user_page.dart

---
+++ lib/ep483_lessons_app/lessons_user_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LessonsUserPage extends StatefulWidget {
+  @override
+  _LessonsUserPageState createState() => _LessonsUserPageState();
+}
+
+class _LessonsUserPageState extends State<LessonsUserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

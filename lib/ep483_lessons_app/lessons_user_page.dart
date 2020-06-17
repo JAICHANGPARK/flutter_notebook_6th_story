842071353a1429e9e2f8491c805d280a054d4448
Dreamwalker <aristojeff@gmail.com>
2020-06-17 20:07:44 +0900

    Update lessons_user_page.dart

--- lib/ep483_lessons_app/lessons_user_page.dart
+++ lib/ep483_lessons_app/lessons_user_page.dart
@@ -162,8 +162,8 @@ class _LessonsUserPageState extends State<LessonsUserPage> {
                                       Expanded(
                                         flex: 1,
                                         child: CircularPercentIndicator(radius: 48,
-                                          lineWidth: 3,
-                                          percent: 0.2,
+                                          lineWidth: 4,
+                                          percent: 0.25,
                                         ),
                                       ),
                                     ],

c67c5b344fa0a4e11c133fddb8839cf684a76e90
Dreamwalker <aristojeff@gmail.com>
2020-06-18 19:36:15 +0900

    Update dmm_main_page.dart

--- lib/ep486_dmm_app/dmm_main_page.dart
+++ lib/ep486_dmm_app/dmm_main_page.dart
@@ -8,3 +8,15 @@ class DmmApp extends StatelessWidget {
     );
   }
 }
+class DmmMainPage extends StatefulWidget {
+  @override
+  _DmmMainPageState createState() => _DmmMainPageState();
+}
+
+class _DmmMainPageState extends State<DmmMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+

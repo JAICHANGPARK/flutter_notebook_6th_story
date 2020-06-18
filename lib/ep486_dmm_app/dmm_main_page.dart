97c74adb8dbd7b8d26d10f7127e5163383ed374f
Dreamwalker <aristojeff@gmail.com>
2020-06-18 19:35:57 +0900

    Update dmm_main_page.dart

--- lib/ep486_dmm_app/dmm_main_page.dart
+++ lib/ep486_dmm_app/dmm_main_page.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class DmmApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp();
   }
 }

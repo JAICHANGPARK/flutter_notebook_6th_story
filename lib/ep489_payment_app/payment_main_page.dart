a30306fa3a4c3ced542b805c094b9d64c5121221
Dreamwalker <aristojeff@gmail.com>
2020-06-21 19:08:20 +0900

    Update payment_main_page.dart

--- lib/ep489_payment_app/payment_main_page.dart
+++ lib/ep489_payment_app/payment_main_page.dart
@@ -55,56 +55,63 @@ class _PaymentMainPageState extends State<PaymentMainPage> {
           height: 72,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Container(
-              height: 64,
-              decoration: BoxDecoration(
-                color: Colors.orange[300],
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "4 ITEMS",
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
+            child: GestureDetector(
+              onTap: (){
+                showDialog(context: context, builder: (context) => AlertDialog(
+                  content: Text("Payment"),
+                ));
+              },
+              child: Container(
+                height: 64,
+                decoration: BoxDecoration(
+                  color: Colors.orange[300],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "4 ITEMS",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        Text.rich(TextSpan(children: [
-                          TextSpan(text: "\$ ", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red)),
-                          TextSpan(
-                              text: "26.00 ",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              )),
-                          TextSpan(text: "Incl taxes", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 12))
-                        ]))
-                      ],
+                          Text.rich(TextSpan(children: [
+                            TextSpan(text: "\$ ", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red)),
+                            TextSpan(
+                                text: "26.00 ",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            TextSpan(text: "Incl taxes", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 12))
+                          ]))
+                        ],
+                      ),
                     ),
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "Pay",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Pay",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        Icon(Icons.play_arrow)
-                      ],
-                    ),
-                  )
-                ],
+                          Icon(Icons.play_arrow)
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),

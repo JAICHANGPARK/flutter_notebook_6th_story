4f9e1ea20177fa5a1950287fe6cd4843d2f7049e
Dreamwalker <aristojeff@gmail.com>
2020-06-21 18:55:41 +0900

    Update payment_main_page.dart

--- lib/ep489_payment_app/payment_main_page.dart
+++ lib/ep489_payment_app/payment_main_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class PaymentMainPage extends StatefulWidget {
+  @override
+  _PaymentMainPageState createState() => _PaymentMainPageState();
+}
+
+class _PaymentMainPageState extends State<PaymentMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

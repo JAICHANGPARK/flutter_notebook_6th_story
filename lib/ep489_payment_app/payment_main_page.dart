50337e171426f539ebee2740bc54ced27e128ae8
Dreamwalker <aristojeff@gmail.com>
2020-06-22 19:42:44 +0900

    Update payment_main_page.dart

--- lib/ep489_payment_app/payment_main_page.dart
+++ lib/ep489_payment_app/payment_main_page.dart
@@ -60,6 +60,7 @@ class _PaymentMainPageState extends State<PaymentMainPage> {
                           Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 16),
                             child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text.rich(TextSpan(
                                   children: [

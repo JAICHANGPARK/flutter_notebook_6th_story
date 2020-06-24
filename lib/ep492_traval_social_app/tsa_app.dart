835bdea26d2e69c8ea14f96b60553ba3172398a9
Dreamwalker <aristojeff@gmail.com>
2020-06-24 20:08:44 +0900

    Update tsa_app.dart

--- lib/ep492_traval_social_app/tsa_app.dart
+++ lib/ep492_traval_social_app/tsa_app.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+
+class TravelSocialMobileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

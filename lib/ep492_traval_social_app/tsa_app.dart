d5ed7440eb10a5d226d41ec3be2d27f3fab5bca9
Dreamwalker <aristojeff@gmail.com>
2020-06-24 20:12:29 +0900

    Update tsa_app.dart

--- lib/ep492_traval_social_app/tsa_app.dart
+++ lib/ep492_traval_social_app/tsa_app.dart
@@ -4,7 +4,41 @@ class TravelSocialMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: TSMMainPage(),
     );
   }
 }
+
+class TSMMainPage extends StatefulWidget {
+  @override
+  _TSMMainPageState createState() => _TSMMainPageState();
+}
+
+class _TSMMainPageState extends State<TSMMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

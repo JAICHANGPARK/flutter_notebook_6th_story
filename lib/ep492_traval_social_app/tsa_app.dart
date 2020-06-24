7ebdb17538254776f56b4e913276ac70113833e7
Dreamwalker <aristojeff@gmail.com>
2020-06-24 20:26:55 +0900

    Update tsa_app.dart

--- lib/ep492_traval_social_app/tsa_app.dart
+++ lib/ep492_traval_social_app/tsa_app.dart
@@ -53,7 +53,12 @@ class _TSMMainPageState extends State<TSMMainPage> {
             ),
             Expanded(
               flex: 8,
-              child: Placeholder(),
+              child: ListView.builder(itemBuilder: (context, index){
+                return Container(
+                  width: MediaQuery.of(context).size.width - 24,
+                  decoration: BoxDecoration(),
+                );
+              }),
             )
           ],
         ),

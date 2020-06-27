5b1058055c2aec238a4f0e555ab85acde62cf3e8
Dreamwalker <aristojeff@gmail.com>
2020-06-27 20:54:10 +0900

    Update delivery_main_page.dart

--- lib/ep495_delivery_app/delivery_main_page.dart
+++ lib/ep495_delivery_app/delivery_main_page.dart
@@ -4,6 +4,20 @@ import 'package:flutter/material.dart';
 class DeliveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DeliveryMainPage(),
+    );
+  }
+}
+
+class DeliveryMainPage extends StatefulWidget {
+  @override
+  _DeliveryMainPageState createState() => _DeliveryMainPageState();
+}
+
+class _DeliveryMainPageState extends State<DeliveryMainPage> {
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }

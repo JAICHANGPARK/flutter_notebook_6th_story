cc7157f2e3c3d24464f67c0c37e8d6eec6d6f2ec
Dreamwalker <aristojeff@gmail.com>
2020-06-30 19:53:56 +0900

    Update travel_app.dart

--- lib/ep498_travel_app/travel_app.dart
+++ lib/ep498_travel_app/travel_app.dart
@@ -18,7 +18,11 @@ class _TravelHomePageState extends State<TravelHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      body: Stack(
+        children: [
+
+        ],
+      ),
     );
   }
 }

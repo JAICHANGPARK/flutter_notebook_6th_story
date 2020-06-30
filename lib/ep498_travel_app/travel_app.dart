dccac407afb15fdfd12d9d8739e92aca48a684e0
Dreamwalker <aristojeff@gmail.com>
2020-06-30 19:53:29 +0900

    Update travel_app.dart

--- lib/ep498_travel_app/travel_app.dart
+++ lib/ep498_travel_app/travel_app.dart
@@ -8,3 +8,16 @@ class TravelApp extends StatelessWidget {
     );
   }
 }
+
+class TravelHomePage extends StatefulWidget {
+  @override
+  _TravelHomePageState createState() => _TravelHomePageState();
+}
+
+class _TravelHomePageState extends State<TravelHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+

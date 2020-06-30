6b90c841ffb7cdf4568d379080f87bc18ad90dc6
Dreamwalker <aristojeff@gmail.com>
2020-06-30 19:56:05 +0900

    Update travel_app.dart

--- lib/ep498_travel_app/travel_app.dart
+++ lib/ep498_travel_app/travel_app.dart
@@ -29,7 +29,11 @@ class _TravelHomePageState extends State<TravelHomePage> {
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.blue,
-                borderRadius: BorderRadius.circular(24)
+                borderRadius: BorderRadius.circular(24),
+                image: DecorationImage(
+                  image: NetworkImage("https://cdn.pixabay.com/photo/2017/02/14/03/03/ama-dablam-2064522_960_720.jpg"),
+                  fit: BoxFit.cover
+                )
               ),
             ),
           )

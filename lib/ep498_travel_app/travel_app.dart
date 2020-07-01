de11411c31b96603b422f209746fc48cd9580713
Dreamwalker <aristojeff@gmail.com>
2020-07-01 19:55:54 +0900

    Update travel_app.dart

--- lib/ep498_travel_app/travel_app.dart
+++ lib/ep498_travel_app/travel_app.dart
@@ -148,7 +148,11 @@ class _TravelHomePageState extends State<TravelHomePage> {
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 0.3, sigmaY: 0.2, ),
                 child: Container(
-                  color: Colors.black.withOpacity(0.2),
+
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(16)
+                  ),
                 ),
               ),
             ),

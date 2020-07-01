cfe9c09d64fa42e814605fe8d68095e014729686
Dreamwalker <aristojeff@gmail.com>
2020-07-01 19:51:48 +0900

    Update travel_app.dart

--- lib/ep498_travel_app/travel_app.dart
+++ lib/ep498_travel_app/travel_app.dart
@@ -42,100 +42,87 @@ class _TravelHomePageState extends State<TravelHomePage> {
             child: Container(
               height: 210,
               child: ListView.builder(
-                itemCount: 10,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index){
-                return Container(
-                  margin: EdgeInsets.only(right: 16),
-                  width: MediaQuery.of(context).size.width - 64,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(24)
-                  ),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        right: 0,
-                        top: 0,
-                        child: Container(
-                          height: 64,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            color: Colors.lightBlueAccent,
-                            borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(16),
-                              topRight: Radius.circular(24)
-                            )
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(right: 16),
+                      width: MediaQuery.of(context).size.width - 64,
+                      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(24)),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            right: 0,
+                            top: 0,
+                            child: Container(
+                              height: 64,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  color: Colors.lightBlueAccent,
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(16), topRight: Radius.circular(24))),
+                              child: Center(
+                                  child: Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              )),
+                            ),
                           ),
-                          child: Center(child: Icon(Icons.arrow_forward, color: Colors.white,)),
-                        ),
-                      ),
-                      Positioned(
-                        top: 16,
-                        left: 16,
-                        bottom: 16,
-                        right: 16,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Newzealand"),
-                            Row(
+                          Positioned(
+                            top: 16,
+                            left: 16,
+                            bottom: 16,
+                            right: 16,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Icon(Icons.map),
-                                Text("20 Destinations")
+                                Text("Newzealand"),
+                                Row(
+                                  children: [Icon(Icons.map), Text("20 Destinations")],
+                                ),
+                                Spacer(),
+                                Container(
+                                  height: 72,
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        child: Container(
+                                          margin: EdgeInsets.all(4),
+                                          decoration: BoxDecoration(
+                                              color: Colors.lightBlue[100], borderRadius: BorderRadius.circular(8)),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          margin: EdgeInsets.all(4),
+                                          decoration: BoxDecoration(
+                                              color: Colors.lightBlue[100], borderRadius: BorderRadius.circular(8)),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          margin: EdgeInsets.all(4),
+                                          decoration: BoxDecoration(
+                                              color: Colors.lightBlue[100], borderRadius: BorderRadius.circular(8)),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          margin: EdgeInsets.all(4),
+                                          decoration: BoxDecoration(
+                                              color: Colors.lightBlue[100], borderRadius: BorderRadius.circular(8)),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
                               ],
                             ),
-                            Spacer(),
-                            Container(
-                              height: 72,
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Container(
-                                      margin: EdgeInsets.all(4),
-                                      decoration: BoxDecoration(
-                                        color: Colors.lightBlue[100],
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Container(
-                                      margin: EdgeInsets.all(4),
-                                      decoration: BoxDecoration(
-                                        color: Colors.lightBlue[100], borderRadius: BorderRadius.circular(8)
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Container(
-                                      margin: EdgeInsets.all(4),
-                                      decoration: BoxDecoration(
-                                        color: Colors.lightBlue[100], borderRadius: BorderRadius.circular(8)
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Container(
-                                      margin: EdgeInsets.all(4),
-                                      decoration: BoxDecoration(
-                                        color: Colors.lightBlue[100],
-                                        borderRadius: BorderRadius.circular(8)
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )
-
-                          ],
-                        ),
-                      )
-
-
-                    ],
-                  ),
-                );
-              }),
+                          )
+                        ],
+                      ),
+                    );
+                  }),
             ),
           ),
           Center(

7f539f48ebdbd160d433be920b2ee7160aff5337
Dreamwalker <aristojeff@gmail.com>
2020-07-01 19:41:03 +0900

    Update travel_app.dart

--- lib/ep498_travel_app/travel_app.dart
+++ lib/ep498_travel_app/travel_app.dart
@@ -21,21 +21,18 @@ class _TravelHomePageState extends State<TravelHomePage> {
     return Scaffold(
       body: Stack(
         children: [
-
           Positioned(
             left: 16,
             right: 16,
             bottom: 16,
-            top:24,
+            top: 24,
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(24),
-                image: DecorationImage(
-                  image: NetworkImage("https://cdn.pixabay.com/photo/2015/03/26/09/41/mountain-690104_960_720.jpg"),
-                  fit: BoxFit.fitHeight
-                )
-              ),
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(24),
+                  image: DecorationImage(
+                      image: NetworkImage("https://cdn.pixabay.com/photo/2015/03/26/09/41/mountain-690104_960_720.jpg"),
+                      fit: BoxFit.fitHeight)),
             ),
           ),
           Positioned(
@@ -48,47 +45,15 @@ class _TravelHomePageState extends State<TravelHomePage> {
             ),
           ),
           Center(
-            child: Text.rich(TextSpan(children:
-            [
-              TextSpan(
-                text: "TRIP  ",
-                style: GoogleFonts.prompt(fontSize: 52,
-                color: Colors.white)
-              ),
-              TextSpan(
-                text: "LO", style: GoogleFonts.prompt(fontSize: 52,
-                  color: Colors.white)
-              ),
-              TextSpan(text: "V", style: GoogleFonts.prompt(fontSize: 52,
-                  color: Colors.lightBlueAccent)),
-              TextSpan(text: "ER", style: GoogleFonts.prompt(fontSize: 52,
-                  color: Colors.white)
-              )
+            child: Text.rich(TextSpan(children: [
+              TextSpan(text: "TRIP  ", style: GoogleFonts.prompt(fontSize: 52, color: Colors.white)),
+              TextSpan(text: "LO", style: GoogleFonts.prompt(fontSize: 52, color: Colors.white)),
+              TextSpan(text: "V", style: GoogleFonts.prompt(fontSize: 52, color: Colors.lightBlueAccent)),
+              TextSpan(text: "ER", style: GoogleFonts.prompt(fontSize: 52, color: Colors.white))
             ])),
           )
-
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
